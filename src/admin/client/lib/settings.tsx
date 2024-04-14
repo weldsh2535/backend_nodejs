3a74175576a05335f91220d74fc41df9ee8453b5
weldsh2535 <weldestadik2535@gmail.com>
2024-04-14 17:10:07 +0300

    update

--- src/admin/client/lib/settings.tsx
+++
@@ -1,3 +0,0 @@
-import Config from "../../../config/admin"
-
-export default Config
--- src/admin/client/lib/text.tsx
+++
@@ -1,6 +0,0 @@
-import locales from "./locales"
-import config from "./settings"
-
-const locale = locales.find(({ local }) => config.language == local)
-
-export default locale
